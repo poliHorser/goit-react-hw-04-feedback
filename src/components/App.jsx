80e8ee74b6bfca86e58b8a45903c35f0db5a47ef
poliHorser <134537683+poliHorser@users.noreply.github.com>
2024-01-14 18:31:27 +0200

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,38 +1,9 @@
-import React, { Component } from 'react';
-import Statistics from './Statistics/Statistics';
-import FeedbackOptions from './FeedBackOptions/FeedbackOptions';
-import Section from './Section/Section';
-import Notification from './Notification/Notification';
+import { useState } from "react"
 
-
-class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
-  handleLeaveFeedback = (type) => {
-    this.setState((prevState) => ({ [type]: prevState[type] + 1 }));
-  };
-  render() {
-    const { good, neutral, bad } = this.state;
-    const totalFeedback = good + neutral + bad;
-
-    const objKey = Object.keys(this.state);
-    return (
-      <div className='all'>
-        <Section title="Please leave your feedback">
-          <FeedbackOptions options={objKey} onLeaveFeedback={this.handleLeaveFeedback} />
-        </Section>
-        <Section  title="Statistics">
-          {totalFeedback > 0 ? (
-            <Statistics good={good} neutral={neutral} bad={bad} />
-          ) : (
-            <Notification  message="There is no feedback" />
-          )}
-        </Section>
-      </div>
-    );
-  }
+const App = () => {
+  useState()
+  return (
+    <div></div>
+  )
 }
-export  {App};
+export default App
