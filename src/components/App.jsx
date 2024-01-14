ca6979e7e22ace5ae974aa50b272cd4810c52dc6
poliHorser <134537683+poliHorser@users.noreply.github.com>
2024-01-14 20:15:03 +0200

    finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,44 @@
 import { useState } from "react"
+import Statistics from './Statistics/Statistics';
+import FeedbackOptions from './FeedBackOptions/FeedbackOptions';
+import Section from './Section/Section';
+import Notification from './Notification/Notification';
 
 const App = () => {
-  useState()
+  
+  const [feedbacks, SetFeedbacks] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  })
+
+  // const [good, setGood] = useState(0)
+  // const [neutral, setNeutral] = useState(0)
+  // const [bad, setBad] = useState(0)
+
+  const handleLeaveFeedback = (type) => {
+    SetFeedbacks((prevFeedbacks) => ({ ...prevFeedbacks, [type]: prevFeedbacks[type] + 1 }))
+  }
+
+  
+  const { good, neutral, bad } = feedbacks;
+  const totalFeedback = good + neutral + bad;
+  const objKey = Object.keys(feedbacks);
+
+  
   return (
-    <div></div>
+  <div className='all'>
+        <Section title="Please leave your feedback">
+          <FeedbackOptions options={objKey} onLeaveFeedback={handleLeaveFeedback} />
+        </Section>
+        <Section  title="Statistics">
+          {totalFeedback > 0 ? (
+            <Statistics good={good} neutral={neutral} bad={bad} />
+          ) : (
+            <Notification  message="There is no feedback" />
+          )}
+        </Section>
+      </div>
   )
 }
-export default App
+export  {App}
